6922f64cedbd7aad816430cf65593994201d0bdb
luffy84 <kjhdinomath@gmail.com>
2025-02-20 06:23:14 +0900

    last

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,12 +1,20 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import { useEffect } from "react";
 
 const PageLayout = ({ children }) => {
-  return (
-    <div className="flex flex-col min-h-screen bg-white">
-      <main className="flex-grow p-6">{children}</main>
-    </div>
-  );
+  useEffect(() => {
+    document.title = "25 퍼플 동박 미궁"; // 제목 변경
+
+    const metaDescription = document.createElement("meta");
+    metaDescription.name = "description";
+    metaDescription.content = "25 새내기들을 위한 퍼플 미니 미궁";
+    document.head.appendChild(metaDescription);
+
+    return () => {
+      document.head.removeChild(metaDescription);
+    };
+  }, []);
+
+  return <>{children}</>;
 };
 
 export default PageLayout;
