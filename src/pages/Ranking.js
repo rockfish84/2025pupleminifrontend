7d2455f7f52ad11a4abf5adb319e16216a7a8d18
luffy84 <kjhdinomath@gmail.com>
2025-03-25 16:13:43 +0900

    rank3

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -4,21 +4,24 @@ const Ranking = () => {
   return (
     <PageLayout>
       <div className="text-center">
-      <br />
-      <br />
+        <br />
+        <br />
         <h2 className="text-3xl font-bold my-6">명예의 전당</h2>
         <p className="text-gray-700">
-          축하합니다! 게임을 클리어한 유저들의 순위입니다. <br/>
-          <br/>
-          <h3 className="text-2xl font-bold mt-6">1위: yuchantony(오유찬)<br/></h3>
-         <br/>
-         퍼플 미궁 신작 언제 나오나요<br/>
-         <br/>
-         <h3 className="text-2xl font-bold mt-6"> 2위: WolfB<br/></h3>
-         <br/>
-          퍼플은내가접수한다.<br/>
-          <br/>
-          - 3위: TBD <br/>
+          축하합니다! 게임을 클리어한 유저들의 순위입니다. <br />
+          <br />
+          <h3 className="text-2xl font-bold mt-6">1위: yuchantony(오유찬)<br /></h3>
+          <br />
+          퍼플 미궁 신작 언제 나오나요<br />
+          <br />
+          <h3 className="text-2xl font-bold mt-6"> 2위: WolfB<br /></h3>
+          <br />
+          퍼플은내가접수한다.<br />
+          <br />
+          <h3 className="text-2xl font-bold mt-6"> 3위: Rain123<br /></h3>
+          <br />
+          너무 재밌게 풀었습니다ㅎㅎ 퍼플 흥해라~ 김정훈 화이팅<br />
+          <br />
         </p>
       </div>
       <br />
